9c25e01cc6c13a069e0e3b9885e3764494179c57
Pavel Shirshov <pshirshov@gmail.com>
2025-01-16 10:07:25 +0000

    improved mac demo

--- 03-macos-system/flake.nix
+++ 03-macos-system/flake.nix
@@ -11,59 +11,71 @@
     darwinConfigurations =
       {
         "demos-iMac-Pro" = darwin.lib.darwinSystem {
-          system = "x86_64-darwin";
+          # change hostname to yours
+          system = "x86_64-darwin"; # for Intel macs
+          #system = "aarch64-darwin"; # for ARM macs
           modules = [
             {
               # all nix-darwin options: https://daiderd.com/nix-darwin/manual/index.html
               system.stateVersion = 5; # a rite, don't think about it
               nixpkgs.config.allowUnfree = true;
+
+              homebrew.enable = false; # you need brew installed to use it through nix
+              homebrew.brews = [ "imagemagick" ];
+              homebrew.casks = [ "firefox" ];
             }
 
             home-manager.darwinModules.home-manager
-            {
-              # all Home Manager options: https://home-manager-options.extranix.com/
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
 
-              # TODO: show how we can use import instead during the demo
-              home-manager.users.demo = { pkgs, lib, ... }: {
-                home.stateVersion = "25.05"; # a rite, don't think about it
-                home.homeDirectory = lib.mkForce "/Users/demo";
+            (
+              let
+                my-user = "demo"; # change username to yours
+              in
+              {
+                # all Home Manager options: https://home-manager-options.extranix.com/
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
 
-                programs.zsh = {
-                  enable = true;
-                };
+                # TODO: show how we can use import instead during the demo
+                home-manager.users."${my-user}" = { pkgs, lib, ... }: {
+                  home.stateVersion = "25.05"; # a rite, don't think about it
+                  home.homeDirectory = lib.mkForce "/Users/${my-user}";
 
-                programs.direnv = {
-                  enable = true;
-                  config = {
-                    # This is unsafe for real workstations. Use subdirectories.
-                    # Slash at the end is important
-                    whitelist.prefix = [ "~/" ];
+                  programs.zsh = {
+                    enable = true;
                   };
-                };
 
-                # nixpkgs packages: https://search.nixos.org/packages?
-                home.packages = with pkgs; [
-                  ammonite
-                ];
+                  programs.direnv = {
+                    enable = true;
+                    config = {
+                      # This is unsafe for real workstations. Use subdirectories.
+                      # Slash at the end is important
+                      whitelist.prefix = [ "~/" ];
+                    };
+                  };
 
-                programs.vscode = {
-                  enable = true; # TODO: explain the Spotlight issues
-                  extensions = with pkgs.vscode-extensions; [
-                    jnoortheen.nix-ide
-                    mkhl.direnv
+                  # nixpkgs packages: https://search.nixos.org/packages?
+                  home.packages = with pkgs; [
+                    ammonite
                   ];
-                  userSettings = {
-                    "workbench.startupEditor" = "newUntitledFile";
-                    "window.openFoldersInNewWindow" = "on";
-                    "telemetry.enableTelemetry" = false;
-                    "telemetry.enableCrashReporter" = false;
-                  };
-                };
 
-              };
-            }
+                  programs.vscode = {
+                    enable = true; # TODO: explain the Spotlight issues
+                    extensions = with pkgs.vscode-extensions; [
+                      jnoortheen.nix-ide
+                      mkhl.direnv
+                    ];
+                    userSettings = {
+                      "workbench.startupEditor" = "newUntitledFile";
+                      "window.openFoldersInNewWindow" = "on";
+                      "telemetry.enableTelemetry" = false;
+                      "telemetry.enableCrashReporter" = false;
+                    };
+                  };
+
+                };
+              }
+            )
           ];
         };
       };
