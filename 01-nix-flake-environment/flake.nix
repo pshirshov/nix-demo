97402b232143b933a022533079b20aafed716a76
Pavel Shirshov <pshirshov@gmail.com>
2025-01-16 10:00:32 +0000

    mixing versions

--- 01-nix-flake-environment/flake.nix
+++ 01-nix-flake-environment/flake.nix
@@ -1,11 +1,14 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/24.05";
 
+  inputs.nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs =
     { self
     , nixpkgs
+    , nixpkgs-unstable
     , flake-utils
     ,
     }:
@@ -13,6 +16,7 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
       in
       {
         devShells.default = pkgs.mkShell {
@@ -20,7 +24,9 @@
             nodejs
             nodePackages.npm
             typescript
-          ];
+          ] ++ (with pkgs-unstable;[
+            scala
+          ]);
 
           shellHook = ''
             export DEMO_TYPESCRIPT=${pkgs.typescript}
@@ -30,4 +36,3 @@
       }
     );
 }
-
